e0989c210b41aa5a65b08053045c8175b0d1550f
endoffline <s1710629025@students.fh-hagenberg.at>
2018-06-12 17:43:47 +0200

    Refactored the navigation bar to its own component

---
+++ vue-pokemon/src/components/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="info">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand to="/"><b-img src="@/assets/logo.png" width="30" height="30" class="d-inline-block align-top" fluid-growth alt="logo"/> Pokemon Tierlist</b-navbar-brand>
+
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item to="/list">List</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navigation'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
