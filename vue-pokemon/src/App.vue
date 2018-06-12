6070d8d7f3d3d1d41e138e1656e43d0742b13a83
endoffline <s1710629025@students.fh-hagenberg.at>
2018-06-12 16:01:13 +0200

    Added bootstrap-vue and navbar

--- vue-pokemon/src/App.vue
+++ vue-pokemon/src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand to="/"><b-img src="./assets/logo.png" width="30" height="30" class="d-inline-block align-top" fluid-growth alt="logo"/> Pokemon Tierlist</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/list">List</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
@@ -18,6 +28,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
