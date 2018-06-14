33368c6e11f772071828c275e21af9f35d578729
Steho <s1710629025@students.fh-hagenberg.at>
2018-06-14 11:40:10 +0200

    pokemon list works and looks now as intended

--- vue-pokemon/src/components/ListItem.vue
+++ vue-pokemon/src/components/ListItem.vue
@@ -1,16 +1,24 @@
 <template>
-  <router-link :to="{ name: 'detail', params: { name: name }}">
-    <div class="pokemon">
-      <img :src="image" height="200"/>
-      {{number}} {{name}} {{types}}
+  <div class="pokemon" @click="showDetails">
+    <img :src="image" height="200"/>
+    <div class="description">
+      {{name}} <br/>
+      <span class="small">#{{number}}
+        <span v-for="type in types" :key="type">{{type}} </span>
+      </span>
     </div>
-  </router-link>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ListItem',
-  props: ['number', 'name', 'types', 'image']
+  props: ['number', 'name', 'types', 'image'],
+  methods: {
+    showDetails: function (event) {
+      this.$router.push({name: 'detail', params: { name: this.name }})
+    }
+  }
 }
 </script>
 
@@ -31,10 +39,37 @@ export default {
     color: #42b983;
   }
   .pokemon {
+    position: relative;
     box-sizing: border-box;
+    cursor: pointer;
   }
   img {
     display: block;
     margin: 0 auto;
+    opacity: 1;
+    transition: .5s ease;
+    backface-visibility: hidden;
+  }
+  .description {
+    transition: .5s ease;
+    opacity: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    text-align: center;
+  }
+
+  .pokemon:hover img {
+    opacity: 0.3;
+  }
+
+  .pokemon:hover .description {
+    opacity: 1;
+  }
+
+  .small {
+    font-size: 13px;
   }
 </style>
