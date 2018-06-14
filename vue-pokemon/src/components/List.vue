fc0ec2c5504932eeb25d4b847191356b84327c56
Steho <s1710629025@students.fh-hagenberg.at>
2018-06-14 14:13:34 +0200

    selecting by type works now

--- vue-pokemon/src/components/List.vue
+++ vue-pokemon/src/components/List.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
+    <div class="types">
+      <b-button @click="updateType(event, '')">All</b-button>
+      <b-button v-for="type in types" :key="type" @click="updateType($event, type)">{{type}}</b-button>
+    </div>
     <div class="list">
       <div v-if="$apollo.loading">Loading...</div>
       <list-item v-for="pokemon in pokemons" :key="pokemon.id" :number="pokemon.number" :name="pokemon.name" :types="pokemon.types" :image="pokemon.image"></list-item>
@@ -23,25 +27,34 @@ export default {
       pokemons: [
         // {id: 1, name: 'Pikachu', type: 'Electric'},
         // {id: 2, name: 'Bulbasaur', type: 'Grass'}
-      ]
+      ],
+      types: ['Normal', 'Fire', 'Water', 'Electric', 'Grass', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying', 'Psychic', 'Bug', 'Rock', 'Ghost', 'Dragon', 'Dark', 'Steel', 'Fairy'],
+      selectedType: ''
     }
   },
   apollo: {
-    pokemons () {
-      return {
-        query: gql`query getPokemonsByType ($first: Int!, $type: String!){
-          pokemons(first: $first, type: $type) {
-            number,
-            name,
-            types,
-            image
-          }
-        }`,
-        variables: {
+    pokemons: {
+      query: gql`query getPokemonsByType ($first: Int!, $type: String!){
+        pokemons(first: $first, type: $type) {
+          number,
+          name,
+          types,
+          image
+        }
+      }`,
+      variables () {
+        return {
           first: 160,
-          type: ''
+          type: this.selectedType
         }
       }
+    }
+  },
+  methods: {
+    updateType: function (event, type) {
+      console.log(type)
+      this.selectedType = type
+      this.$apollo.queries.pokemons.refetch()
     }
   }
 }
