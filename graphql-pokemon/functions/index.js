540f7598d42e99363de4861a3bf4bbe63cd03da9
ComanderIke <kepplinger.daniel@gmail.com>
2018-06-15 19:43:17 +0200

    added firebase tools

---
+++ graphql-pokemon/functions/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+require('babel-polyfill');
+
+const functions = require('firebase-functions');
+
+
+
+var _app = require('./app');
+
+var _app2 = _interopRequireDefault(_app);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; } // eslint-disable-line import/no-extraneous-dependencies
+
+var PORT = 80;
+
+_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+            switch (_context.prev = _context.next) {
+                case 0:
+                    _context.next = 2;
+                    return _app2.default.listen(PORT);
+
+                case 2:
+
+                    console.log('GraphQL-Pokemon started on http://localhost:' + PORT + '/');
+
+                case 3:
+                case 'end':
+                    return _context.stop();
+            }
+        }
+    }, _callee, undefined);
+}))();
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
